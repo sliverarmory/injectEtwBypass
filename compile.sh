5df477cc0244246d82fdac1e2e9dd0700f7657a9
moloch-- <875022+moloch--@users.noreply.github.com>
2021-12-30 12:08:08 -0600

    Added ci/cd workflow

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-x86_64-w64-mingw32-gcc -m64 -mwindows -c injectEtwBypass.c -o injectEtwBypass.o -masm=intel -Wall -fno-asynchronous-unwind-tables -nostdlib -fno-ident -Wl,-Tlinker.ld,--no-seh
-echo "Succesfully compiled, you can load 'injectEtwBypass.cna' into Cobalt-Strike and use the command 'injectEtwBypass PID'"
+#!/bin/bash
+x86_64-w64-mingw32-gcc -m64 -mwindows -c injectEtwBypass.c -o ./compiled/injectEtwBypass.o -masm=intel -Wall -fno-asynchronous-unwind-tables -nostdlib -fno-ident -Wl,-Tlinker.ld,--no-seh
